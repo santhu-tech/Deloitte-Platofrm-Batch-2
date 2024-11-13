a1d3495a0cb0ee29f904037c159e85b57f92ed50
santhu-tech <psanthiyamahe@gmail.com>
2024-11-13 14:32:55 +0530

    Add files via upload

--- EmpDBwith Error Handling/EmpDB/src/main/java/com/employee/EmpDB/Model/ErrorResponse.java
+++ EmpDBwith Error Handling/EmpDB/src/main/java/com/employee/EmpDB/Model/ErrorResponse.java
@@ -1,43 +1,62 @@
-//package com.employee.EmpDB.Model;
-//
-//import java.time.LocalDateTime;
-//
-//public class ErrorResponse {
-//
-//    // private LocalDateTime timestamp;
-//    private String HttpStatus status;
-//  //  private String error;
-//    private String message;
-//    private String path;
-//
-//
-//    public ErrorResponse(int status, String message, String path) {
-//        this.status = status;
-//        this.message = message;
-//        this.path = path;
-////    }
-//
-//    public int getStatus() {
-//        return status;
-//    }
+package com.employee.EmpDB.Model;
 
-//    public void setStatus(int status) {
-//        this.status = status;
-//    }
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+public class ErrorResponse {
 //
-//    public String getMessage() {
-//        return message;
-//    }
-//
-//    public void setMessage(String message) {
-//        this.message = message;
-//    }
-//
-//    public String getPath() {
-//        return path;
-//    }
-//
-//    public void setPath(String path) {
-//        this.path = path;
-//    }
-//}
+    private LocalDateTime timestamp;
+    private int status;   //404,401,200
+    private String error;
+    private String message;
+    private String path;  // /api/employees/id
+
+    public ErrorResponse(HttpStatus status, String message, String path) {
+        this.timestamp = LocalDateTime.now();
+        this.status = status.value();
+        this.error = status.getReasonPhrase();
+        this.message = message;
+        this.path = path;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
